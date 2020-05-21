d8f3db828cca3dc78694680cb311ca7ed6c14799
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2020-05-21 14:37:02 -0500

    Bug fix: fix index typo output_bead, output_spring, attempt 2.

--- src/filament.cpp
+++ src/filament.cpp
@@ -257,7 +257,7 @@ vector<vector<double>> filament::output_beads(int fil)
     vector<vector<double>> out;
     for (size_t i = 0; i < beads.size(); i++) {
         out.push_back(beads[i]->output());
-        out[i].push_back(double(i));
+        out[i].push_back(double(fil));
     }
     return out;
 }
@@ -267,7 +267,7 @@ vector<vector<double>> filament::output_springs(int fil)
     vector<vector<double>> out;
     for (size_t i = 0; i < springs.size(); i++) {
         out.push_back(springs[i]->output());
-        out[i].push_back(double(i));
+        out[i].push_back(double(fil));
     }
     return out;
 }
